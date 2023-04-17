19df56a1df4fa87aa518ffa872030225d6739c5a
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-17 11:38:34 +0300

    Добавьте файлы проекта.

---
+++ CSLighrtMedcinePoliclinic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSLighrtMedcinePoliclinicN8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int queueOldLadies;
+            int timeReceipt = 10;
+            int oneHour = 60;
+            int hours;
+            int minutes;
+
+            Console.Write("Введите количество старушек: ");
+            queueOldLadies = Convert.ToInt32(Console.ReadLine());
+            hours = queueOldLadies * timeReceipt / oneHour;
+            minutes = queueOldLadies * timeReceipt % oneHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди: " +
+                              $"{hours} часа и {minutes} минут.");
+            Console.ReadLine();
+        }
+    }
+}
